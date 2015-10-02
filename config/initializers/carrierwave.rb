ee15c3e57e4c721a287dd5cc062551114fdd1939
kohrVid <kohrVid@zoho.com>
2015-10-02 18:14:20 +0100

    Still attempting to fix an issue with the image uploader in production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,14 +4,10 @@ if Rails.env.production?
 	          :provider		 => 'AWS',
 	          :aws_access_key_id	 => ENV['S3_ACCESS_KEY'],
 		  :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-		  :region		 => 'eu-central-1',
-		  :x_amz_algorithm	 => 'AWS4-HMAC-SHA256',
-		  'x-amz-server-side-encryption-customer-algorithm' => 'AES256',
-		        'x-amz-server-side-encryption-customer-key' => '************************************',
-			      'x-amz-server-side-encryption-customer-key-md5' => '************************************'
-			        
+		  :region		 => 'eu-central-1'
 }
 		config.cache_dir = "#{Rails.root}/tmp/uploads"
 		config.fog_directory	 =  ENV['S3_BUCKET']
 	end
+
 end
