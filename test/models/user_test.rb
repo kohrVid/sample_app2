b938fe59eddc7a424dfa6295525888273dd5fb7c
kohrVid <kohrVid@zoho.com>
2015-10-03 13:12:44 +0100

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
 		    password: "foobar", password_confirmation: "foobar")
@@ -76,6 +72,35 @@ class UserTest < ActiveSupport::TestCase
     @user.microposts.create!(content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
       @user.destroy
+    end
+  end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+   #posts from followed user 
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    #posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    #posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
     end
   end
 
